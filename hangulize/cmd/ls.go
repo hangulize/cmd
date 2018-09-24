7c539294bd9c66b1c8419d5f41ef18bd9c5ad925
Heungsub Lee <sub@subl.ee>
2018-09-25 02:02:42 +0900

    ls command handles HGLs

--- hangulize/cmd/ls.go
+++ hangulize/cmd/ls.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"os"
+
 	"github.com/spf13/cobra"
 
 	"github.com/hangulize/hangulize"
@@ -11,21 +13,50 @@ func init() {
 }
 
 var lsCmd = &cobra.Command{
-	Use:   "ls",
-	Short: "List of bundled lang specs",
+	Use:   "ls [HGL...]",
+	Short: "List of bundled specs or given HGLs",
+	Args:  cobra.MinimumNArgs(0),
 	Run: func(cmd *cobra.Command, args []string) {
 		template := "%-8s %-8s %-24s %-24s\n"
 
 		cmd.Printf(template, "LANG", "STAGE", "ENG", "KOR")
 
-		for _, lang := range hangulize.ListLangs() {
-			spec, _ := hangulize.LoadSpec(lang)
+		for _, spec := range listSpecs(args) {
 			cmd.Printf(template,
-				lang,
+				spec.Lang.ID,
 				spec.Config.Stage,
 				spec.Lang.English,
 				spec.Lang.Korean,
 			)
 		}
 	},
+}
+
+func listSpecs(args []string) []*hangulize.Spec {
+	specs := make([]*hangulize.Spec, 0)
+
+	if len(args) == 0 {
+		// Load bundled specs.
+		for _, lang := range hangulize.ListLangs() {
+			spec, _ := hangulize.LoadSpec(lang)
+			specs = append(specs, spec)
+		}
+	} else {
+		// Load specs from HGL files.
+		for _, name := range args {
+			file, err := os.Open(name)
+			if err != nil {
+				continue
+			}
+
+			spec, err := hangulize.ParseSpec(file)
+			if err != nil {
+				continue
+			}
+
+			specs = append(specs, spec)
+		}
+	}
+
+	return specs
 }
